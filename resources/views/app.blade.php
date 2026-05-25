94b269baf6b970bf7407d9cbe8b47c2f6cb76bf4
Abdulhadi Ahmad <abdulherdy101@gmail.com>
2026-05-25 20:54:42 +0100

    feat: include Ziggy routes in blade template for client-side routing support

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title inertia>{{ config('app.name', 'YipOnline E-commerce') }}</title>
     
+    @routes
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @inertiaHead
 </head>
