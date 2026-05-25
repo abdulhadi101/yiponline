ef57aefd90b6a20e7f21467c1a1cadfca1b3bee1
Abdulhadi Ahmad <abdulherdy101@gmail.com>
2026-05-25 20:37:48 +0100

    chore: move faker to production dependencies and prevent migration errors from blocking container startup

--- docker/start.sh
+++ docker/start.sh
@@ -19,7 +19,7 @@ php artisan route:cache || true
 php artisan view:cache || true
 
 if [ "$RUN_MIGRATIONS" = "true" ]; then
-  php artisan migrate --seed --force
+  php artisan migrate --seed --force || echo "Migration failed, continuing..."
 fi
 
 exec supervisord -c /etc/supervisor/conf.d/supervisord.conf
